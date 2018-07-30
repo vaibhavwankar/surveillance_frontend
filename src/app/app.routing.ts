7a609a61504985ba1a9b44aa1905ccbe66c57f6c
vaibhavwankar <wankar333@gmail.com>
2018-07-30 18:05:30 +0530

    "added buy sell component with dialog functionality"

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,16 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+import { BuysellComponent } from './components/buysell/buysell.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     {
-         path: 'dashboard', component: DashboardComponent,
-         children: [
+        path: 'dashboard', component: DashboardComponent,
+        children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-             { path: 'home', component: HomeComponent },
-    //         { path: 'create', component: CreatePatientComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'buysell', component: BuysellComponent },
         ]
     },
 
